5bcb74bf9b58f36e5fe73cc1da21ba424c4598e4
github username <github email address>
2019-12-01 10:34:30 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def index
     @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |cat_attr|
+      if cat_attr["name"].present?
+        category = Category.find_or_create_by(cat_attr)
+        self.categories << category
+      end
+    end
+  end
 end
